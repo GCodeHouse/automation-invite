6cafd1eb1c754f1b787431d0199118918a649c12
Rizel Scarlett <blackgirlbytes@github.com>
2022-02-25 03:20:31 +0000

    invitations bug fixed

--- webhook-handlers/handle-comments-on-issue.js
+++ webhook-handlers/handle-comments-on-issue.js
@@ -1,13 +1,14 @@
 
 module.exports = handleCommentingOnIssue;
 const { Octokit } = require("@octokit/core");
+// const octokit = new Octokit();
 
-async function handleCommentingOnIssue(context) {
+async function handleCommentingOnIssue(app, context) {
     const comment = context.payload.comment
-    await checkCommentAuthor(comment, octokit, context)
+    await checkCommentAuthor(comment, context)
 }
 
-async function checkCommentAuthor(comment, octokit, context) {
+async function checkCommentAuthor(comment, context) {
     const admins = ['blackgirlbytes'];
     const pendingMaintainerArray = context.payload.issue.title.match(/\@(.*)/)
     const pendingMaintainerUsername = pendingMaintainerArray[1]
@@ -16,31 +17,33 @@ async function checkCommentAuthor(comment, octokit, context) {
     const commentAuthor = comment.user.login
     if (admins.includes(commentAuthor)) {
         console.log('an admin commented on this issue')
-        await checkCommentBody(comment, octokit, context, pendingMaintainerUsername)
+        await checkCommentBody(comment, context, pendingMaintainerUsername)
     } else if (commentAuthor === pendingMaintainerUsername) {
         console.log('this is not a comment from an admin')
     }
 }
 
-async function checkCommentBody(comment, octokit, context, pendingMaintainerUsername) {
+async function checkCommentBody(comment, context, pendingMaintainerUsername) {
     const commentBody = comment.body.toUpperCase();
-
+    console.log('show me the comment body', commentBody)
     if (commentBody === 'APPROVED') {
         await addUserToTeam(pendingMaintainerUsername)
     }
 }
 
 async function addUserToTeam(pendingMaintainerUsername) {
-    const octokit = new Octokit({
+    const { personalAccessTokenData } = await require("./ssm-handler");
+    const myOctokit = new Octokit({
         // pass in GitHub personal access token
-        auth: 'personal_access_token',
+        auth: await personalAccessTokenData,
     });
-    const result = await octokit.request('PUT /orgs/{org}/teams/{team_slug}/memberships/{pendingMaintainerUsername}', {
-        org: 'gcodehouse',
-        team_slug: 'mentors',
+    const result = await myOctokit.request('PUT /orgs/{org}/teams/{team_slug}/memberships/{username}', {
+        org: 'GCodeHouse',
+        team_slug: 'Mentors',
         username: pendingMaintainerUsername,
         role: 'member',
     }).catch(async (err) => {
+        console.log('this is an error', err)
         if (err.status === 403) {
             console.log(`Forbidden ${err.status}`)
         }
